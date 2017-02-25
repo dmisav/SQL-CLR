604001977974f777e5f3dd989ea8176f8ae08f7b
dmisav <savchenko.di@gmail.com>
2017-02-25 12:54:24 +0200

    added procedure usage

---
+++ UserDefinedFunctions/UserDefinedFunctions/UserDefinedStoredProcedures.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace UserDefinedFunctions
+{
+    //CLR functions are more appropriate than simple Transact-SQL statements for computation-intensive applications.
+    public class UserDefinedStoredProcedures
+    {
+        //The following shows a stored procedure returning information through an OUTPUT parameter:
+        [SqlProcedure]
+        public static void PriceSum(out SqlInt32 value)
+        {
+            using (SqlConnection connection = new SqlConnection("context connection=true"))
+            {
+                value = 0;
+                connection.Open();
+                SqlCommand command = new SqlCommand("select id from [dbo].[Address]", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        value += reader.GetSqlInt32(0);
+                    }
+                }
+            }
+        }
+    }
+}
